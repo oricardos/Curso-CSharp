2b37cda66bcbc7e160ad88a2b2ce4fa98ee4ea8e
oricardos <ricardo.rs95@gmail.com>
2025-10-25 12:09:07 -0300

    Exercicio heranca e polimorfismo 1

--- HerancaPolimorfismo/Entities/Employee.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HerancaPolimorfismo.Entities
-{
-    public class Employee
-    {
-        public string Name { get; set; }
-        public int Hours { get; set; }
-        public double ValuePerHour { get; set; }
-    }
-}

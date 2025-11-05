6eeffa679c8d8737619caa29d1b52c0b1e9be780
Luiz Ricardo Silva <46203839+oricardos@users.noreply.github.com>
2025-11-05 15:46:30 -0300

    FileStream e StreamReader
    Updated file handling method to use StreamReader instead of FileInfo for reading lines from a file.

--- Files/Files/Program.cs
+++ Files/Files/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -10,25 +10,53 @@ namespace HelloWorld
     {
         public static void Main(string[] args)
         {
-            string sourcePath = @"c:\projetos\file1.txt";
-            string targetPath = @"c:\projetos\file2.txt";
+            // FILES
+            //string sourcePath = @"c:\projetos\file1.txt";
+            //string targetPath = @"c:\projetos\file2.txt";
+
+            //try
+            //{
+            //    //https://learn.microsoft.com/en-us/dotnet/api/system.io.fileinfo?view=net-9.0#methods
+            //    FileInfo fileInfo = new FileInfo(sourcePath);
+            //    fileInfo.CopyTo(targetPath);
+
+            //    string[] lines = File.ReadAllLines(sourcePath);
+            //    foreach (string line in lines)
+            //    {
+            //        Console.WriteLine(line);
+            //    }
+            //}
+            //catch (IOException e)
+            //{
+            //    Console.WriteLine("An error occurred");
+            //    Console.WriteLine(e.Message);
+            //}
+
+            //FILESTREAM
+            //https://learn.microsoft.com/pt-br/dotnet/api/system.io.filestream?view=net-8.0
+
+            string path = @"c:\projetos\file1.txt";
+            StreamReader sr = null;
 
             try
             {
-                //https://learn.microsoft.com/en-us/dotnet/api/system.io.fileinfo?view=net-9.0#methods
-                FileInfo fileInfo = new FileInfo(sourcePath);
-                fileInfo.CopyTo(targetPath);
+                sr = File.OpenText(path);
 
-                string[] lines = File.ReadAllLines(sourcePath);
-                foreach (string line in lines)
+                while (!sr.EndOfStream)
                 {
+                    string line = sr.ReadLine();
                     Console.WriteLine(line);
                 }
+
             }
             catch (IOException e)
             {
                 Console.WriteLine("An error occurred");
                 Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
             }
         }
     }

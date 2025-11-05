3c37bd1ee07cc77b5b3787be636d239744ba36f9
Luiz Ricardo Silva <46203839+oricardos@users.noreply.github.com>
2025-11-05 14:10:04 -0300

    Trabalhando com arquivos

---
+++ Files/Files/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string sourcePath = @"c:\projetos\file1.txt";
+            string targetPath = @"c:\projetos\file2.txt";
+
+            try
+            {
+                //https://learn.microsoft.com/en-us/dotnet/api/system.io.fileinfo?view=net-9.0#methods
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}

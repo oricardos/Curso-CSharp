45b787108059978e9ba7a06ca82c2c9e75c6c0cd
oricardos <ricardo.rs95@gmail.com>
2026-01-08 14:30:23 -0300

    projeto editor de texto

---
+++ balta.io/TextEditor/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.IO;
+
+static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("O que você deseja fazer?");
+    Console.WriteLine("1 - Abrir arquivo");
+    Console.WriteLine("2 - Criar novo arquivo");
+    Console.WriteLine("0 - Sair");
+    short option = short.Parse(Console.ReadLine());
+
+    switch (option)
+    {
+        case 0:
+            System.Environment.Exit(0);
+            break;
+        case 1:
+            OpenFile();
+            break;
+        case 2:
+            NewFile();
+            break;
+        default:
+            Menu();
+            break;
+    }
+}
+
+static void OpenFile()
+{
+    Console.Clear();
+    Console.WriteLine("Qual caminho para do arquivo?");
+    string path = Console.ReadLine();
+
+    using (var file = new StreamReader(path))
+    {
+        string text = file.ReadToEnd();
+        Console.WriteLine(text);
+    }
+    
+    Console.WriteLine("");
+    Console.ReadLine();
+    Menu();
+}
+
+static void NewFile()
+{
+    Console.Clear();
+    Console.WriteLine("Digite seu texto abaixo: (ESC para sair)");
+    Console.WriteLine("------------------------");
+
+    string text = "";
+
+    do
+    {
+        text += Console.ReadLine();
+        text += Environment.NewLine;
+    }
+    while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+    Save(text);
+
+}
+
+static void Save(string text)
+{
+    Console.Clear();
+    Console.WriteLine("Qual caminho para salvar o arquivo?");
+
+    var path = Console.ReadLine();
+
+    using (var file = new StreamWriter(path)) 
+    {
+        file.Write(text);
+    }
+
+    Console.WriteLine($"Arquivo {path} salvo com sucesso");
+    Console.ReadLine();
+    Menu();
+}
+
+Menu();

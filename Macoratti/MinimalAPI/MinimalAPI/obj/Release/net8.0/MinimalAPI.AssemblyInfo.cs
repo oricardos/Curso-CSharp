ddaaade12ddedbbcb48a0a727b19c7f00e287e30
oricardos <ricardo.rs95@gmail.com>
2026-01-14 11:20:15 -0300

    .

--- Macoratti/MinimalAPI/MinimalAPI/obj/Release/net8.0/MinimalAPI.AssemblyInfo.cs
+++ Macoratti/MinimalAPI/MinimalAPI/obj/Release/net8.0/MinimalAPI.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinimalAPI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f3e0887a894760384ed2b732d35be4a3e728395")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinimalAPI")]

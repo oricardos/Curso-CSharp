2fb494ce759506a7ad3e837da3b047dc5830b574
oricardos <ricardo.rs95@gmail.com>
2026-01-15 10:57:18 -0300

    criando classe de contexto

--- Macoratti/APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ Macoratti/APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddaaade12ddedbbcb48a0a727b19c7f00e287e30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69277a4627bcaa59c7ee603be840e122df1e2ff5")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

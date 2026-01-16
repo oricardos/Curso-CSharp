dbd15d421df8933707738445fbc95ed0cce6b142
oricardos <ricardo.rs95@gmail.com>
2026-01-16 11:25:25 -0300

    git commit -m "Resolve merge conflict by incorporating both suggestions"

--- Macoratti/APICatalogo/APICatalogo/Models/Categoria.cs
+++ Macoratti/APICatalogo/APICatalogo/Models/Categoria.cs
@@ -5,4 +5,5 @@ public class Categoria
     public int CategoriaId { get; set; }
     public string? Nome { get; set; }
     public string? ImagemUrl { get; set; }
+    public ICollection<Produto> Produtos { get; set; }
 }

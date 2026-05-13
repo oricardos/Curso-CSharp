92683f9bb64b060f3d4be78d465381c30ccd2244
oricardos <ricardo.rs95@gmail.com>
2026-05-12 22:21:57 -0300

    Add custom error messages to Produto validation attributes
    Improved user feedback by specifying custom error messages for the [Required] attributes on Nome, Descricao, Preco, and ImagemUrl properties in the Produto class. This ensures clearer validation messages when required fields are missing.

--- Macoratti/APICatalogo/APICatalogo/Models/Produto.cs
+++ Macoratti/APICatalogo/APICatalogo/Models/Produto.cs
@@ -8,19 +8,19 @@ public class Produto
 {
     public int ProdutoId { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "O Nome é obrigatório")]
     [StringLength(80)]
     public string? Nome { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "A Descrição é obrigatória")]
     [StringLength(300)]
     public string? Descricao { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "O Preço é obrigatório")]
     [Column(TypeName ="decimal(10,2)")]
     public decimal Preco { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "A Imagem é obrigatória")]
     [StringLength(300)]
     public string? ImagemUrl { get; set; }
     public float Estoque { get; set; }

981766c187debd98d4a0a86b711b4e8f7a13d623
oricardos <ricardo.rs95@gmail.com>
2026-07-09 23:34:54 -0300

    Update assembly version and related cache files
    Updated AssemblyInformationalVersionAttribute to a new commit hash in APICatalogo.AssemblyInfo.cs. Refreshed build cache files (APICatalogo.AssemblyInfoInputs.cache and rpswa.dswa.cache.json) to reflect the new version information.

--- Macoratti/APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ Macoratti/APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97b451e572a772ccdd31fe22e9a992646da3eba3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd4d17b287a217d7a513b4e15a5e971a7424f6ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
